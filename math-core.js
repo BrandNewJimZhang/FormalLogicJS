64139da73dde9c9ff2dd0907ecc3cf07f6831ae3
BrandNewJimZhang <dqzhw@126.com>
2021-05-08 11:45:15 +0800

    add truth table

--- math-core.js
+++ math-core.js
@@ -1,5 +1,5 @@
 //
-// MathematicalLogicJS (a.k.a. MathLogicJS)
+// FormalLogicJS (a.k.a. MathematicalLogicJS, MathLogicJS)
 //
 // Copyright 2021 (c) Jim Zhang
 // 
@@ -43,6 +43,210 @@ $(function() {
             // deactivate element after setting it into its new location
             selectedElement = null;  
         });
+    };
+
+    function truth_table(input) {
+        function init(arr, n) {
+            for(let i=0;i<n;i++) arr[i]=0;
+        }
+
+        var value=new Array();
+        init(value, 50);
+        var flag=1;
+        var input;
+        var table="<table class=\'tg\'><thead><tr>";
+        var box, len_val=0, len_input=0;
+
+        function is_alpha(ch){
+            if(!(ch=='∧'||ch=='∨'||ch=='('||ch==')'||ch=='~'||ch=='→'||ch=='↔')){
+                return true;
+            }
+            return false;
+        }
+
+        function in_value(ch){
+            for(let i=0;i<len_val;i++){
+                if(value[i]==ch){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        var ans=new Array();
+        init(ans,50);
+        function rang(num){
+            if(num>=len_val){
+                table+="<tr>"
+                for(let i=0;i<len_val;i++){
+                    table+="<td class=\'tg-0lax\'>";
+                    if(ans[i]==1){
+                        table+="T";
+                    }
+                    else{
+                        table+="F";
+                    }
+                    table+="</td>";
+                }
+                table+="<td>";
+                if(R()==true){
+                    table+="T";
+                }
+                else{
+                    table+='F';
+                }
+                table+="</td></tr>"
+                return ;
+            }
+            ans[num]=0;
+            rang(num+1);
+            ans[num]=1;
+            rang(num+1);
+            return;
+        }
+
+        var cal = new Array();
+        var num = new Array();
+        var top_num, top_cal;
+
+        function in_var(ch){
+            for(let i=0;i<len_val;i++){
+                if(value[i]==ch)return true;
+            }
+            return false;
+        }
+        function find_var(ch){
+            for(let i=0;i<len_val;i++){
+                if(value[i]==ch){
+                    if(ans[i]==1){
+                        return '1';
+                    }
+                    else{
+                        return '0';
+                    }
+                }
+            }
+        }
+        function calculate(){
+            if(cal[top_cal]=="~"){
+                if(num[top_num]=='1'){
+                    num[top_num]='0';
+                }
+                else{
+                    num[top_num]='1';
+                }
+            }
+            else if(cal[top_cal]=='∧'){
+                let ch1=num[top_num];
+                let ch2=num[top_num-1];
+                top_num--;
+                if(ch1=='1'&&ch2=='1') num[top_num]='1';
+                else num[top_num]='0';
+            }
+            else if(cal[top_cal]=='∨'){
+                let ch1=num[top_num];
+                let ch2=num[top_num-1];
+                top_num--;
+                if(ch1=='0'&&ch2=='0')num[top_num]='0';
+                else num[top_num]='1';
+            }
+            else if(cal[top_cal]=='→'){
+                let ch1=num[top_num];
+                let ch2=num[top_num-1];
+                top_num--;
+                if(ch1=='0'&&ch2=='1')num[top_num]='0';
+                else num[top_num]='1';
+            }
+            else if(cal[top_cal]=='↔'){
+                let ch1=num[top_num];
+                let ch2=num[top_num-1];
+                top_num--;
+                if((ch1=='0'&&ch2=='0')||(ch1=='1'&&ch2=='1'))num[top_num]='1';
+                else num[top_num]='0';
+            }
+            top_cal--;
+        }
+        function compare(a,b){
+            if(b=="(") return true;
+            if(a=="~"&&b!="~") return true;
+            return false;
+        }
+
+        function R(){
+            //return true;
+            init(cal, 200);
+            init(num, 200);
+            top_cal = -1; top_num = -1;
+            for(let i=0;i<len_input;i++){
+                if(in_var(box[i]))num[++top_num]=find_var(box[i]);
+                else{
+                    if(box[i]==")"){
+                        while (cal[top_cal]!="(") {
+                            calculate();
+                        }top_cal--;
+                    }
+                    else if(box[i]=="("){
+                        cal[++top_cal]=box[i];
+                    }
+                    else if(top_cal==-1 || compare(box[i],cal[top_cal])){
+                        cal[++top_cal]=box[i];
+                    }
+                    else{
+                        while(!(top_cal==-1 || compare(box[i],cal[top_cal]))){
+                            calculate();
+                        }
+                        cal[++top_cal]=box[i];
+                    }
+                }
+            }
+            while(top_cal!=-1){
+                calculate();
+            }
+            if(!(top_num==0&&top_cal==-1))flag=0;
+            if(num[0]=='1')return true;
+            return false;
+        }
+
+        document.getElementById("truth-table").innerHTML=undefined;
+        if(input==""){
+            document.getElementById("truth-table").innerText="There is no input";
+            return;
+        }
+        box=[...input];//['p','&','q']
+        /*var box1=[];
+        var len=box.length;
+        for(let i=0;i<len;i++){
+            if(box[i]!=' ')box1.push(box[i])
+        }*/
+
+        
+        //document.getElementById("display").innerText=box1;
+        //return;
+        
+        len_input=box.length;//3
+
+        for(let i=0;i<len_input;i++){
+            if(is_alpha(box[i])&&(!in_value(box[i]))){
+                value[len_val]=box[i];
+                len_val++;
+                table+="<th class=\'tg-l6li\'>"
+                table=(table+box[i]);
+                table+="</th>"
+            }
+            
+        }
+        table+="<th>Result</th>"
+        table+="</tr></thead><tbody>"//add body
+
+        rang(0);
+
+        if(flag==0){
+            document.getElementById("truth-table").innerText="Invalid input"
+        }
+        else{
+            table+="</table>"
+            document.getElementById("truth-table").innerHTML=table;
+        }
     };
 
     var mubu = makeSVG('svg', {
@@ -161,11 +365,12 @@ $(function() {
                 y2: parseInt($(temp[0]).attr('y'))+parseInt($(temp[0]).attr('height')/2),
                 stroke: '#000'
             })
+            var raw_text = '(~'+temp[1]+')';
             var text = $(makeSVG('text', {
                 x: 15,
                 y: 40*item + 30,
                 fill: 'transparent',
-            })).text('(~'+temp[1]+')');
+            })).text(raw_text);
             mubu.append(text);
             mubu.append($(makeSVG('rect', {
                 x: parseInt($(temp[0]).attr('x'))+parseInt($(temp[0]).attr('width'))+100,
@@ -176,13 +381,14 @@ $(function() {
                 fill: '#fff',
                 rx: 5,
                 ry: 5,
-                text: '(~'+temp[1]+')',
+                text: raw_text,
             })));
             var text = $(makeSVG('text', {
                 x: parseInt($(temp[0]).attr('x'))+parseInt($(temp[0]).attr('width'))+105,
                 y: parseInt($(temp[0]).attr('y')) + 20,
                 fill: '#000',
-            })).text('(~'+temp[1]+')');
+            })).text(raw_text);
+            console.log(raw_text)
             mubu.append(text);
             mubu.append(line);
             mubu.append(rectbox);
@@ -262,10 +468,20 @@ $(function() {
                 y: ((parseInt($(first[0]).attr('y'))+parseInt($(second[0]).attr('y'))))/2 + 20,
                 fill: '#000',
             })).text(raw_text);
-
             mubu.append(rectbox, text, line1, line2, line_together_h, line_together_v);
-        }
+            $('#hidden-container').text(raw_text);
+        };
     });
+
+    item = $("<button>Generate truth table</button>").attr({
+        id: 'truth-table-button',
+    }).click(function () {
+        var text = $('#hidden-container').text();
+        console.log(text);
+        truth_table(text);
+    });
+    
+    divinput.append(item);
 });
 
 
